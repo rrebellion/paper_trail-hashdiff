de405fecf8833ad9479afdd6723ee1f19ba5e05c
artinens <artinens@yandex.ru>
2024-05-31 17:05:48 +0300

    added a possibility to work with StoreModel
    Signed-off-by: artinens <artinens@yandex.ru>

--- lib/paper_trail_hashdiff.rb
+++ lib/paper_trail_hashdiff.rb
@@ -12,12 +12,15 @@ class PaperTrailHashDiff
   def diff(changes)
     diff_changes = {}
     changes.each do |field, value_changes|
+      old_value = convert_to_hash(value_changes[0])
+      new_value = convert_to_hash(value_changes[1])
+
       diff_changes[field] =
         if !only_objects || (
-          value_changes[0] && value_changes[1] &&
-          (value_changes[0].is_a?(Hash) || value_changes[0].is_a?(Array))
+          old_value && new_value &&
+          (old_value.is_a?(Hash) || new_value.is_a?(Array))
         )
-          Hashdiff.diff(value_changes[0], value_changes[1], array_path: true, use_lcs: false)
+          Hashdiff.diff(old_value, new_value, array_path: true, use_lcs: false)
         else
           value_changes
         end
@@ -35,5 +38,13 @@ class PaperTrailHashDiff
 
   def load_changeset(version)
     HashWithIndifferentAccess.new(version.object_changes_deserialized)
+  end
+
+  private
+
+  def convert_to_hash(value)
+    return value unless defined?(StoreModel::Model) && value.is_a?(StoreModel::Model)
+
+    value.as_json
   end
 end
