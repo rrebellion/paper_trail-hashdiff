b3486a0d5d7a7850028b2ad0530fc6057e4eccda
artinens <artinens@yandex.ru>
2026-03-03 15:09:53 +0300

    [DEV-5294] fix diff when no diff (nil -> nil)
    Signed-off-by: artinens <artinens@yandex.ru>

--- lib/paper_trail_hashdiff.rb
+++ lib/paper_trail_hashdiff.rb
@@ -15,15 +15,15 @@ class PaperTrailHashDiff
       old_value = convert_to_hash(value_changes[0])
       new_value = convert_to_hash(value_changes[1])
 
-      diff_changes[field] =
-        if !only_objects || (
-          old_value && new_value &&
-          (old_value.is_a?(Hash) || new_value.is_a?(Array))
-        )
-          Hashdiff.diff(old_value, new_value, array_path: true, use_lcs: false)
-        else
-          value_changes
-        end
+      if !only_objects || (
+        old_value && new_value &&
+        (old_value.is_a?(Hash) || new_value.is_a?(Array))
+      )
+        result = Hashdiff.diff(old_value, new_value, array_path: true, use_lcs: false)
+        diff_changes[field] = result unless result.empty?
+      else
+        diff_changes[field] = value_changes
+      end
     end
     diff_changes
   end
